367c710138035d4cf3f58d986d6587a96be05429
Noah Davis <sc23nd@leeds.ac.uk>
2024-11-26 11:30:13 +0000

    Improved card re-arrangement when deleting a ticket

--- ticketApp/main/app/static/js/delete_ticket.js
+++ ticketApp/main/app/static/js/delete_ticket.js
@@ -23,11 +23,13 @@ async function query_server(ticket_id){
 
         const data = await response.json(); // Parse JSON response from the server
         if (data.success) {
-            // delete the ticket from the webpage
+            // Find the ticket card by ID
             var ticketCard = document.getElementById('card-' + ticket_id);
-            if (ticketCard) {
-                ticketCard.remove();
+            if (ticketCard && ticketCard.parentNode) {
+                // Remove the parent div of the ticket card
+                ticketCard.parentNode.remove();
             }
+        
         } else {
             //console.log(message);
            alert('error');
@@ -35,5 +37,37 @@ async function query_server(ticket_id){
     } catch (error) {
         //console.error('Error sending ticket_id code to server:', error);
         //showPopup('An unexpected error occurred while sending the QR code.', 'error');
+    }
+    reArrange();
+}
+
+
+function reArrange(){
+    // get all the cards
+    const cards = document.querySelectorAll('.active-ticket')
+    
+    // remove these cards from the html
+    for (let ticket of cards){
+        ticket.remove();
+    }
+
+    // now re-enter these tickets in the correct placement
+    var count = 0;
+    const newRow = document.createElement('div');
+    newRow.className = 'row';
+    ticket_list = document.getElementById('ticket-list');
+    ticket_list.appendChild(newRow); // this line
+    for (let ticket of cards){
+        if (count == 2){
+            // now we need a new row element
+            const newRow = document.createElement('div');
+            newRow.className = 'row';
+            ticket_list.appendChild(newRow);
+            count = 0; // Reset count after creating a new row
+        }
+        ticket.classList.add('d-flex');
+        ticket.classList.remove('d-none');
+        newRow.appendChild(ticket);
+        count += 1;
     }
 }
