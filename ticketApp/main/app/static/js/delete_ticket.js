88682c93635eec3d557063aca5b2aef701b8374e
Noah Davis <sc23nd@leeds.ac.uk>
2024-11-28 18:20:35 +0000

    Added some improvements to cards re-arranging in index.html

--- ticketApp/main/app/static/js/delete_ticket.js
+++ ticketApp/main/app/static/js/delete_ticket.js
@@ -25,6 +25,7 @@ async function query_server(ticket_id){
         if (data.success) {
             // Find the ticket card by ID
             var ticketCard = document.getElementById('card-' + ticket_id);
+            var ticketCardParent = ticketCard.parentNode;
             if (ticketCard && ticketCard.parentNode) {
                 // Remove the parent div of the ticket card
                 ticketCard.parentNode.remove();
@@ -38,13 +39,17 @@ async function query_server(ticket_id){
         //console.error('Error sending ticket_id code to server:', error);
         //showPopup('An unexpected error occurred while sending the QR code.', 'error');
     }
-    reArrange();
+    const classNames = ticketCardParent.className.split(' ');
+    const lastClassName = classNames[classNames.length - 1];
+    reArrange(lastClassName);
 }
 
 
-function reArrange(){
+function reArrange(ticket_class){
+    console.log(ticket_class);
     // get all the cards
-    const cards = document.querySelectorAll('.active-ticket')
+    const cards = document.querySelectorAll('.' + ticket_class);
+    console.log(cards);
     
     // remove these cards from the html
     for (let ticket of cards){
@@ -52,6 +57,38 @@ function reArrange(){
     }
 
     // now re-enter these tickets in the correct placement
+    var count = 0;
+    const newRow = document.createElement('div');
+    newRow.className = 'row';
+    if (ticket_class == 'active-ticket'){
+    ticket_list = document.getElementById('ticket-list');
+    } else {
+        ticket_list = document.getElementById('old-tickets');
+    }
+    ticket_list.appendChild(newRow); // this line
+    for (let ticket of cards){
+        if (count == 2){
+            // now we need a new row element
+            const newRow = document.createElement('div');
+            newRow.className = 'row';
+            ticket_list.appendChild(newRow);
+            count = 0; // Reset count after creating a new row
+        }
+        ticket.classList.add('d-flex');
+        ticket.classList.remove('d-none');
+        newRow.appendChild(ticket);
+        count += 1;
+    }
+}
+
+function reArrangeUsed(){
+    // get all the used cards
+    const cards = document.querySelectorAll('.used-ticket');
+    console.log(cards);
+    for (let ticket of cards){
+        ticket.remove();
+    }
+    var count = 0;
     var count = 0;
     const newRow = document.createElement('div');
     newRow.className = 'row';
@@ -70,4 +107,5 @@ function reArrange(){
         newRow.appendChild(ticket);
         count += 1;
     }
+
 }
